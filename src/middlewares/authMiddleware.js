e06b1d2de286b425e1207d4b6bae3338640d16fe
DeadNord <eosipopo@gmail.com>
2022-04-08 16:53:12 +0300

    add favorite

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,24 +1,15 @@
 const { User } = require("../models/index");
 const { Unauthorized } = require("http-errors");
-const { SECRET_KEY } = process.env;
+const { JWT_SECRET_KEY } = process.env;
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = async (req, res, next) => {
-  // const { authorization = "" } = req.headers;
-  // const { bearer, token } = authorization.split(" ");
   const [, token] = req.headers["authorization"].split(" ");
 
-  // console.log(bearer);
-  // console.log(token);
   try {
-    // if (bearer !== "Bearer") {
-    //   // throw new Unauthorized("Not authorized");
-    //   res.status(401).json(Unauthorized("Not authorized"));
-    // }
-    const { id } = jwt.verify(token, SECRET_KEY);
+    const { id } = jwt.verify(token, JWT_SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token) {
-      // throw new Unauthorized("Not authorized");
       res.status(401).json(Unauthorized("Not authorized"));
     }
     req.user = user;
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,4 @@
 const authMiddleware = require("./authMiddleware");
 const validationMiddleware = require("./validationMiddleware");
-const controllerWrapper = require("./controllerWrapper");
 
-module.exports = { authMiddleware, validationMiddleware, controllerWrapper };
-
-// const { users: ctrl } = require("..");
+module.exports = { authMiddleware, validationMiddleware };
